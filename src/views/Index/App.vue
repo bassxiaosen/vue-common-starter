c14afc501679d52d941e99bd37565bf5e58037ec
cai.yusen <314121378@qq.com>
2019-02-12 15:02:04 +0800

    use vuex

--- src/views/Index/App.vue
+++ src/views/Index/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    首页首页
+  <div class="asdasd">
+    首页首页aaa
     {{ shit }}
+    <br>
+    {{ count }}
+    <button @click="$store.commit('increment')">点我a</button>
   </div>
 </template>
-<style lang="less">
-
+<style lang="less" scoped>
+  .asdasd {
+    color: #aaaaaa;
+  }
 </style>
 <script>
 export default {
+  components: {},
   data() {
     return {
-      shit: '1'
+      shit: '1啊啊撒旦asdasdasdf'
+    }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count
     }
   }
 }
--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -2,6 +2,11 @@ import Vue from "vue"
 import Router from "vue-router"
 import App from "./App"
 
+
+import store from "./stores"
+
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store,
 }).$mount('#app')
